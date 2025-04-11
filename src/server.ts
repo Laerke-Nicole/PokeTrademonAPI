b51a5df477a3c47f34c2e945882c36514a1aea7f
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2025-04-11 15:30:37 +0200

    port adaptation to render

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app"; // ✅ Use `app` directly
 
-const PORT = process.env.PORT || 5004;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
