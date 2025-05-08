879a35c9dffcbdc2f4c2d8edba97dbb90ee86601
Lærke Nielsen <laenie01@easv365.dk>
2025-05-08 19:21:17 +0200

    added more data to news

--- src/interfaces/News.ts
+++ src/interfaces/News.ts
@@ -1,8 +1,12 @@
+import { IUser } from './User';
+
 export interface News extends Document {
     id: string;
     title: string;
     subTitle: string;
     text: string;
     imageURL: string;
+    date: string;
+    theme: string;
     ishidden: boolean;
 }
--- src/models/NewsModel.ts
+++ src/models/NewsModel.ts
@@ -6,8 +6,7 @@ const newsSchema = new Schema<News>({
     subTitle: { type: String, required: true, min: 6, max: 255 },
     text: { type: String, required: true, min: 6, max: 1024 },
     imageURL: { type: String, required: true },
-    ishidden: { type: Boolean, required: false },
-    _createdBy: { type: String, ref: 'User', required: true }
+    ishidden: { type: Boolean, required: false }
 });
 
 
