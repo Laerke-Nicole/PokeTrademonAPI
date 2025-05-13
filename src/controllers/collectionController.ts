e808d58b9b1759bb2ae6fce1f5ecb41260556864
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2025-05-13 11:15:57 +0200

    user deleted

--- src/controllers/collectionController.ts
+++ src/controllers/collectionController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import User from "../models/User";
+import UserModel from "../models/UserModel";
 
 export const addCardToCollection = async (req: Request, res: Response) => {
   try {
     const { userId, cardId } = req.body;
-    const user = await User.findById(userId);
+    const user = await UserModel.findById(userId);
     if (!user) return res.status(404).json({ message: "User not found" });
 
     user.collection.push(cardId);
