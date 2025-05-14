0044feeef9ae4813ad0a3c0f5754d8dad5a15812
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2025-05-14 23:23:10 +0200

    test added: tradeUtils

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-const { createDefaultPreset } = require("ts-jest/presets")
-
-const tsJestTransformCfg = createDefaultPreset().transform
-
-/** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
+  roots: ["<rootDir>/src/tests"],
   transform: {
-    ...tsJestTransformCfg,
+    "^.+\\.ts$": "ts-jest"
   },
+  moduleFileExtensions: ["ts", "js", "json"],
+  testMatch: ["**/*.test.ts"]
 };
