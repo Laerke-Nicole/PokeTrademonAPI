6c708b32156a9e95b5e120e0761d84ff985f3667
Lærke Nielsen <laenie01@easv365.dk>
2025-05-17 23:09:33 +0200

    made about us interface and model

---
+++ src/interfaces/AboutUs.ts
@@ -0,0 +1,16 @@
+export interface AboutUs extends Document {
+  id: string;
+  aboutUsTitle: string;
+  aboutUsText: string;
+  mission: string;
+  vision: string;
+  valuesSubTitle: string;
+  valueOneTitle: string;
+  valueOne: string;
+  valueTwoTitle: string;
+  valueTwo: string;
+  valueThreeTitle: string;
+  valueThree: string;
+  imageURL: string;
+  userId: string;
+}
---
+++ src/models/AboutUsModel.ts
@@ -0,0 +1,51 @@
+import { Schema, model, Query } from 'mongoose';
+import { AboutUs } from '../interfaces/AboutUs';
+
+const aboutUsSchema = new Schema<AboutUs>({
+    aboutUsTitle: { type: String, required: true, min: 2, max: 100 },
+    aboutUsText: { type: String, required: true, min: 6, max: 1024 },
+    mission: { type: String, required: true, min: 2, max: 100 },
+    vision: { type: String, required: true, min: 2, max: 100 },
+    valuesSubTitle: { type: String, required: true, min: 2, max: 100 },
+    valueOneTitle: { type: String, required: true, min: 2, max: 100 },
+    valueOne: { type: String, required: true, min: 2, max: 100 },
+    valueTwoTitle: { type: String, required: true, min: 2, max: 100 },
+    valueTwo: { type: String, required: true, min: 2, max: 100 },
+    valueThreeTitle: { type: String, required: true, min: 2, max: 100 },
+    valueThree: { type: String, required: true, min: 2, max: 100 },
+    imageURL: { type: String, required: true },
+    userId: { type: String, required: true },
+});
+
+
+// define how its being updated in mongoose
+type UpdateQuery<T> = {
+    [key: string]: unknown;
+} & {
+    __v?: number;
+    $set?: Partial<T> & { __v?: number };
+    $setOnInsert?: Partial<T> & { __v?: number };
+    $inc?: { __v?: number };
+};
+
+
+// define about us schema in mongoose  
+aboutUsSchema.pre<Query<AboutUs, AboutUs>>('findOneAndUpdate', function () {
+    const update = this.getUpdate() as UpdateQuery<AboutUs>;
+    if (update.__v != null) {
+        delete update.__v;
+    }
+    const keys: Array<'$set' | '$setOnInsert'> = ['$set', '$setOnInsert'];
+    for (const key of keys) {
+        if (update[key] != null && update[key]!.__v != null) {
+            delete update[key]!.__v;
+            if (Object.keys(update[key]!).length === 0) {
+                delete update[key];
+            }
+        }
+    }
+    update.$inc = update.$inc || {};
+    update.$inc.__v = 1;
+});
+
+export const aboutUsModel = model<AboutUs>("AboutUs", aboutUsSchema);
