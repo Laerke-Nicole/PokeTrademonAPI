779846cd12658dd22d144636b3f26a3f1f60ff63
Lærke Nielsen <laenie01@easv365.dk>
2025-05-21 23:09:36 +0200

    recaptcha removal

--- src/utils/validationSchemas.ts
+++ src/utils/validationSchemas.ts
@@ -6,8 +6,6 @@ export const validateUserRegistration = (data: IUser) => {
     username: Joi.string().min(3).max(255).required(),
     email: Joi.string().email().min(6).max(255).required(),
     password: Joi.string().min(6).max(20).required(),
-
-    recaptchaToken: Joi.string().optional(),
   });
   return schema.validate(data);
 };
